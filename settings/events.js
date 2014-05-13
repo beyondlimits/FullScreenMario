6a3e64db3a2498a12f0b8afcfec37b0cb82eda62
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-05-13 15:29:57 -0400

    Created settings dir for FullScreenMario settings
    The sprites.js and events.js files moved there, though events do not yet
    work.

---
+++ settings/events.js
@@ -0,0 +1,6 @@
+FullScreenMario.prototype.events = {
+    onSpriteCycleStart: "onadding",
+    doSpriteCycleStart: "placed",
+    cycleCheckValidity: "alive",
+    timingDefault: 9
+};
--- settings/sprites.js
+++ settings/sprites.js
@@ -763,16 +763,3 @@ FullScreenMario.prototype.sprites = {
           }
     }
 };
-
-// Resets the main canvas and context
-function resetCanvas() {
-  // The global canvas is one that fills the screen
-  window.canvas = getCanvas(innerWidth, innerHeight, true);
-  window.context = canvas.getContext("2d");
-  body.appendChild(canvas);
-}
-
-function resetLibrary() {
-    window.PixelRender = FSM.PixelRender;
-    window.PixelDrawer = FSM.PixelDrawer;
-}
