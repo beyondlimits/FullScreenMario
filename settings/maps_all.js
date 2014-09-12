d385ac9ea752ad798a37a023bb1a48f938c503a0
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-09-12 13:04:16 -0400

    Added 6-3
    Mostly checked... sort of.

--- settings/maps_all.js
+++ settings/maps_all.js
@@ -2147,5 +2147,66 @@
                 ]
             }
         ]
+    },
+    {
+        "name": "6-3",
+        "time": 300,
+        "locations": [
+            { "entry": "Plain" }
+        ],
+        "areas": [
+            {
+                "setting": "Overworld Night Alt2",
+                "creation": [
+                    { "location": 0 },
+                    { "macro": "Castle" },
+                    { "macro": "Pattern", "pattern": "BackCloud", "y": 4, "repeat": 4 },
+                    { "macro": "Floor", "width": 128 },
+                    { "macro": "Tree", "x": 144, "width": 24 },
+                    { "macro": "Tree", "x": 168, "y": 32, "width": 24 },
+                    { "macro": "Tree", "x": 192, "width": 24 },
+                    { "thing": "Platform", "x": 224, "width": 16, "floating": true, "start": 0, "end": 56 },
+                    { "macro": "Fill", "thing": "Coin", "x": 226, "y": 87, "xnum": 2, "xwidth": 8 },
+                    { "macro": "Tree", "x": 248, "y": 32, "width": 32 },
+                    { "macro": "Tree", "x": 296, "width": 24 },
+                    { "thing": "Springboard", "x": 304, "y": 14.5 },
+                    { "macro": "Tree", "x": 344, "width": 24 },
+                    { "macro": "Fill", "thing": "Coin", "x": 345, "y": 71, "xwidth": 7, "xwidth": 8 },
+                    { "thing": "Platform", "x": 348, "y": 64, "width": 16, "sliding": true, "start": 312, "end": 364 },
+                    { "thing": "Platform", "x": 388, "y": 47, "width": 16, "sliding": true, "start": 356, "end": 400 },
+                    { "macro": "Tree", "x": 392, "y": 16, "width": 32 },
+                    { "thing": "Block", "x": 440, "y": 80, "contents": "Mushroom" },
+                    { "thing": "Platform", "x": 444, "y": 56, "width": 16, "sliding": true, "start": 408, "end": 464 },
+                    { "thing": "Platform", "x": 480, "y": 7, "width": 16, "floating": true, "start": -8, "end": 52 },
+                    { "macro": "Tree", "x": 520, "y": 32, "width": 40 },
+                    { "macro": "Scale", "x": 572, "y": 86 },
+                    { "macro": "Scale", "x": 636, "y": 86 },
+                    { "macro": "Tree", "x": 680, "y": 24, "width": 40 },
+                    { "macro": "Tree", "x": 680, "y": 80, "width": 24 },
+                    { "macro": "Fill", "thing": "Coin", "x": 681, "y": 87, "xnum": 3, "xwidth": 8 },
+                    { "macro": "Tree", "x": 720, "y": 40, "width": 24 },
+                    { "macro": "Tree", "x": 744, "y": 0, "width": 24 },
+                    { "macro": "Tree", "x": 776, "y": 0, "width": 32 },
+                    { "macro": "fill", "thing": "Coin", "x": 801, "y": 39, "xnum": 4, "xwidth": 8 },
+                    { "macro": "Tree", "x": 824, "width": 24 },
+                    { "macro": "Tree", "x": 856, "y": 32, "width": 40 },
+                    { "macro": "Tree", "x": 904, "width": 40 },
+                    { "thing": "Springboard", "x": 928, "y": 14.5 },
+                    { "thing": "Platform", "x": 972, "y": 63, "width": 32, "sliding": true, "start": 940, "end": 992 },
+                    { "macro": "Tree", "x": 984, "width": 24 },
+                    { "macro": "Scale", "x": 1020, "y": 86 },
+                    { "macro": "Tree", "x": 1056, "width": 32 },
+                    { "macro": "Tree", "x": 1056, "y": 64, "width": 24 },
+                    { "macro": "Tree", "x": 1080, "y": 32, "width": 32 },
+                    { "thing": "Platform", "x": 1128, "y": 47, "xnum": 2, "xwidth": 8 },
+                    { "thing": "Platform", "x": 1192, "y": 39, "width": 16, "falling": true },
+                    { "thing": "Platform", "x": 1223, "y": 47, "width": 16, "falling": true },
+                    { "macro": "Fill", "thing": "Coin", "x": 1233, "y": 79, "xnum": 2, "xwidth": 8 },
+                    { "macro": "Tree", "x": 1238, "y": 64, "width": 24 },
+                    { "macro": "Floor", "x": 1280, "width": 256 },
+                    { "macro": "EndOutsideCastle", "x": 1332 }
+                ]
+            }
+        ]
     }
 ]);
