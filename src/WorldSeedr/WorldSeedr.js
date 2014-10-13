50d63fffc42a8eeae014509d04a311dedc9cb539
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-10-12 22:41:48 -0400

    Added & utilized position shrinking utilities
    This way it's not hardcoding ".left +-= child.width" stuff.

--- src/WorldSeedr/WorldSeedr.js
+++ src/WorldSeedr/WorldSeedr.js
@@ -52,6 +52,7 @@ function WorldSeedr(settings) {
      */
     function generateContentChildren(schema, position) {
         var contents = schema.contents,
+            direction = contents.direction,
             positionSaved = positionSave(position),
             leftSave = position.left,
             children, 
@@ -60,19 +61,20 @@ function WorldSeedr(settings) {
         switch(contents.mode) {
             case "Certain":
                 children = contents.children.map(function (choice) {
-                    child = parseChoice(choice, position);
-                    position.left = child.right;
+                    child = parseChoice(choice, position, direction);
+                    shrinkPositionByChild(position, child, direction);
                     return child;
                 });
                 break;
             case "Random":
                 children = [];
                 while(position.left < position.right) {
-                    child = generateChild(contents, position);
+                    child = generateChild(contents, position, direction);
                     if(!child) {
                         break;
                     }
-                    position.left = child.right;
+                    shrinkPositionByChild(position, child, direction);
+                    debugger;
                     children.push(child);
                 }
                 break;
@@ -92,7 +94,7 @@ function WorldSeedr(settings) {
     /**
      * 
      */
-    function generateChild(contents, position) {
+    function generateChild(contents, position, direction) {
         var choice = chooseAmongPosition(contents.children, position),
             schema;
         
@@ -111,7 +113,7 @@ function WorldSeedr(settings) {
     /**
      * 
      */
-    function parseChoice(choice, position) {
+    function parseChoice(choice, position, direction) {
         var schema = all_possibilities[choice.title],
             width = (choice.arguments && choice.arguments.width) 
                 ? choice.arguments.width 
@@ -125,7 +127,9 @@ function WorldSeedr(settings) {
                 "right": position.left + width,
                 "type": "Known",
                 "title": choice.title,
-                "arguments": schema.arguments ? chooseAmongPosition(schema.arguments, position) : undefined
+                "arguments": schema.arguments 
+                    ? chooseAmongPosition(schema.arguments, position) 
+                    : undefined
             };
         }
         
@@ -176,6 +180,7 @@ function WorldSeedr(settings) {
     function chooseAmongPosition(children, position) {
         var width = position.right - position.left,
             height = position.top - position.bottom;
+        
         return chooseAmong(children.filter(function (choice) {
             choice = all_possibilities[choice.title];
             return doesChoiceFit(choice, width, height);
@@ -261,6 +266,33 @@ function WorldSeedr(settings) {
             if(positionSaved.hasOwnProperty(i)) {
                 position[i] = positionSaved[i];
             }
+        }
+    }
+    
+    /**
+     * Shrinks a position by the size of a child, in a particular direction.
+     * 
+     * @param {Object} position   An object that contains .left, .right, .top, 
+     *                            and .bottom.
+     * @param {Object} child   An object that contains .left, .right, .top, and
+     *                         .bottom.
+     * @param {String} direction   A string direction to shrink the position by:
+     *                             "top", "right", "bottom", or "left".
+     */
+    function shrinkPositionByChild(position, child, direction) {
+        switch(direction) {
+            case "top":
+                position.bottom = child.top;
+                break;
+            case "right":
+                position.left = child.right;
+                break;
+            case "bottom":
+                position.top = child.bottom;
+                break;
+            case "left":
+                position.right = child.left;
+                break;
         }
     }
     
