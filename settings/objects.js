5eed6fc140dfaab08fa91bee63e1c194a2984058
Josh Goldberg <diogenesthecynic@hotmail.com>
2014-12-18 18:23:35 -0500

    Fix for Vine topheights
    There was a gap.

--- settings/objects.js
+++ settings/objects.js
@@ -628,6 +628,7 @@
             },
             Vine: {
                 width: 7,
+                topheight: 7,
                 repeat: true,
                 nofall: true,
                 nocollide: true,
