fcc2125475c56d6ac1105c1d2b3c4ade43a65f02
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-05-14 12:30:53 -0400

    Fix for platforms always having"scale" attribute
    The macro now places them as having "inScale", so as to not conflict
    with the rendering engine's .scale.

--- FullScreenMario.js
+++ FullScreenMario.js
@@ -2101,6 +2101,7 @@ var FullScreenMario = (function(GameStartr) {
      * @param {Thing} thing
      */
     function spawnMoveFloating(thing) {
+        console.log("Moving", thing.title);
         // Make sure thing.begin <= thing.end
         thing.EightBitter.setMovementEndpoints(thing);
         
@@ -7731,7 +7732,7 @@ var FullScreenMario = (function(GameStartr) {
                 "x": x - (widthLeft / 2), 
                 "y": y - dropLeft, 
                 "width": widthLeft,
-                "scale": true,
+                "inScale": true,
                 "tension": (dropLeft - 1.5) * unitsize,
                 "onThingAdd": spawnScalePlatform,
                 "collectionName": collectionName,
@@ -7742,7 +7743,7 @@ var FullScreenMario = (function(GameStartr) {
                 "x": x + between - (widthRight / 2),
                 "y": y - dropRight, 
                 "width": widthRight,
-                "scale": true,
+                "inScale": true,
                 "tension": (dropRight - 1.5) * unitsize,
                 "onThingAdd": spawnScalePlatform,
                 "collectionName": collectionName,
