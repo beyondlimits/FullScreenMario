8fa8bb8ec5b492cebb23a5772b59ab4bb69622a2
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-06-19 02:58:37 -0400

    Re-added Source/index.js
    The .gitignore was ignoring it accidentally. Whoops

---
+++ Source/index.js
@@ -0,0 +1,12 @@
+﻿document.onreadystatechange = function (event) {
+    if (event.target.readySate !== "complete") {
+        return;
+    }
+
+    var time = Date.now(),
+        UserWrapper = new UserWrappr.UserWrappr(FullScreenMario.FullScreenMario.prototype.proliferate({
+            "GameStartrConstructor": FullScreenMario.FullScreenMario
+        }, FullScreenMario.FullScreenMario.settings.ui, true));
+
+    console.log("It took " + (Date.now() - time) + " milliseconds to start."), UserWrapper.displayHelpMenu()
+};
