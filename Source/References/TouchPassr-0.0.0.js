68e7af2626543ed3b60eb9f64a04093b906f2ff5
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-06-24 21:57:51 -0400

    Added TouchPassr-0.0.0.js reference to index.html

---
+++ Source/References/TouchPassr-0.0.0.js
@@ -0,0 +1,22 @@
+/**
+* TouchPass is an in-progress module.
+* This means it doesn't yet exist as its own repository.
+* When it is ready, it will be spun off into its own repository on GitHub.
+*/
+var TouchPassr;
+(function (_TouchPassr) {
+    "use strict";
+
+    /**
+    *
+    */
+    var TouchPassr = (function () {
+        /**
+        * @param {ITouchPassrSettings} settings
+        */
+        function TouchPassr(settings) {
+        }
+        return TouchPassr;
+    })();
+    _TouchPassr.TouchPassr = TouchPassr;
+})(TouchPassr || (TouchPassr = {}));
--- Source/References/TouchPassr-0.0.0.ts
+++ Source/References/TouchPassr-0.0.0.ts
@@ -14,6 +14,9 @@ module TouchPassr {
         
     }
     
+    /**
+     * 
+     */
     export class TouchPassr {
         
         
