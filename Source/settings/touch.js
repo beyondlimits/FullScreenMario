12ad60147a8e7dcfb4adb31b91db71c5fb47992f
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-06-25 16:37:22 -0400

    Added schema proposal for touch.js
    Seems like it should be future-proof. Hopefully.

---
+++ Source/settings/touch.js
@@ -0,0 +1,168 @@
+FullScreenMario.FullScreenMario.settings.touch = {
+    "inputs": [
+        {
+            "name": "Joystick",
+            "type": "joystick",
+            "position": {
+                "vertical": "bottom",
+                "horizontal": "left",
+                "offset": {
+                    "left": 35,
+                    "top": -35
+                }
+            },
+            "directions": [
+                {
+                    "name": "Up",
+                    "neighbors": ["UpLeft", "UpRight"]
+                },
+                {
+                    "name": "UpRight",
+                    "neighbors": ["Up", "Right"],
+                    "pipes": {
+                        "activated": {
+                            "onkeydown": ["right"]
+                        },
+                        "deactivated": {
+                            "onkeyup": ["right"]
+                        }
+                    }
+                },
+                {
+                    "name": "Right",
+                    "neighbors": ["UpRight", "DownRight"],
+                    "pipes": {
+                        "activated": {
+                            "onkeydown": ["right"]
+                        },
+                        "deactivated": {
+                            "onkeyup": ["right"]
+                        }
+                    }
+                },
+                {
+                    "name": "DownRight",
+                    "neighbors": ["Right", "Down"],
+                    "pipes": {
+                        "activated": {
+                            "onkeydown": ["down", "right"]
+                        },
+                        "deactivated": {
+                            "onkeyup": ["down", "right"]
+                        }
+                    }
+                },
+                {
+                    "name": "Down",
+                    "neighbors": ["DownRight", "DownLeft"],
+                    "pipes": {
+                        "activated": {
+                            "onkeydown": ["down"]
+                        },
+                        "deactivated": {
+                            "onkeyup": ["down"]
+                        }
+                    }
+                },
+                {
+                    "name": "DownLeft",
+                    "neighbors": ["Down", "left"],
+                    "pipes": {
+                        "activated": {
+                            "onkeydown": ["down", "left"]
+                        },
+                        "deactivated": {
+                            "onkeyup": ["down", "left"]
+                        }
+                    }
+                },
+                {
+                    "name": "Left",
+                    "neighbors": ["DownLeft", "UpLeft"],
+                    "pipes": {
+                        "activated": {
+                            "onkeydown": ["left"]
+                        },
+                        "deactivated": {
+                            "onkeyup": ["left"]
+                        }
+                    }
+                },
+                {
+                    "name": "UpLeft",
+                    "neighbors": ["Left", "Up"],
+                    "pipes": {
+                        "activated": {
+                            "onkeydown": ["left"]
+                        },
+                        "deactivated": {
+                            "onkeyup": ["left"]
+                        }
+                    }
+                }
+            ]
+        },
+        {
+            "name": "A",
+            "type": "Button",
+            "label": "A",
+            "position": {
+                "vertical": "bottom",
+                "horizontal": "right",
+                "offset": {
+                    "left": -28,
+                    "top": -49
+                }
+            },
+            "pipes": {
+                "activated": {
+                    "onkeydown": ["up"]
+                },
+                "deactivated": {
+                    "onkeydown": ["up"]
+                }
+            }
+        },
+        {
+            "name": "B",
+            "type": "Button",
+            "label": "B",
+            "position": {
+                "vertical": "bottom",
+                "horizontal": "right",
+                "offset": {
+                    "left": -49,
+                    "top": -28
+                }
+            },
+            "pipes": {
+                "activated": {
+                    "onkeydown": ["sprint"]
+                },
+                "deactivated": {
+                    "onkeydown": ["sprint"]
+                }
+            }
+        },
+        {
+            "name": "Start",
+            "type": "Button",
+            "label": "Start",
+            "position": {
+                "vertical": "bottom",
+                "horizontal": "center",
+                "offset": {
+                    "top": -14
+                }
+            },
+            "pipes": {
+                "activated": {
+                    "onkeydown": ["pause"]
+                },
+                "deactivated": {
+                    "onkeydown": ["pause"]
+                }
+            }
+        }
+    ]
+};
