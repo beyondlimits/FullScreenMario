063ab761f8510fa71e2234c4c816d962c9715a0e
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-06-24 21:45:37 -0400

    Added initial TouchPassr-0.0.0.ts

---
+++ Source/References/TouchPassr-0.0.0.ts
@@ -0,0 +1,27 @@
+/**
+ * TouchPass is an in-progress module.
+ * This means it doesn't yet exist as its own repository.
+ * When it is ready, it will be spun off into its own repository on GitHub.
+ */
+module TouchPassr {
+    "use strict";
+    
+    export interface ITouchPassrSettings {
+        
+    }
+    
+    export interface ITouchPassr {
+        
+    }
+    
+    export class TouchPassr {
+        
+        
+        /**
+         * @param {ITouchPassrSettings} settings
+         */
+         constructor(settings: ITouchPassrSettings) {
+             
+         }
+    }
+}
