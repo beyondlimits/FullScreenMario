9b9700241c32b2b0e1f5853d1070dab4bef94c51
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-07-07 07:15:42 -0700

    Changed index.js to record more accurate time diff
    It starts recording before the onreadystatechange callback.

--- Source/index.js
+++ Source/index.js
@@ -1,10 +1,12 @@
-﻿document.onreadystatechange = function (event) {
+﻿var time = Date.now();
+
+document.onreadystatechange = function (event) {
     if (event.target.readyState !== "complete") {
         return;
     }
 
-    var time = Date.now(),
-        UserWrapper = new UserWrappr.UserWrappr(FullScreenMario.FullScreenMario.prototype.proliferate({
+    var UserWrapper = new UserWrappr.UserWrappr(FullScreenMario.FullScreenMario.prototype.proliferate(
+        {
             "GameStartrConstructor": FullScreenMario.FullScreenMario
         }, FullScreenMario.FullScreenMario.settings.ui, true));
 
